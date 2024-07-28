d23d80d2b14e5a4a4a1cc506835cd85d63d9f1c0
Tuhba <ozkayikcitugba@gmail.com>
2024-07-28 13:40:14 +0300

    Add project files.

---
+++ WebSite/Controllers/AnaSayfaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models.Siniflar;
+
+namespace WebSite.Controllers
+{
+    public class AnaSayfaController : Controller
+    {
+        // GET: AnaSayfa
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger = c.AnaSayfas.ToList();
+            return View(deger);
+        }
+        public PartialViewResult icon()
+        {
+            var deger = c.icons.ToList();
+            return PartialView(deger);
+        }
+
+    }
+}
---
+++ WebSite/Models/Siniflar/icon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models.Siniflar
+{
+    public class icon
+    {
+        [Key]
+        public int id { get; set; }
+        public string ikon { get; set; }
+        public string link { get; set;}
+    }
+}
